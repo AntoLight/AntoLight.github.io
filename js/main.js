c3cda692d1f7e57d8b803e75bd1bdb1579e05093
AntoLight <antonioglorioso1@hotmail.com>
2020-07-25 21:14:42 -0300

    7.1 Update
    Little changes that match more the theme design

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ $(document).ready(function () {
                                 );
                         }
                 },
-                offset: 450,
+                offset: 850,
         });
 
         //Web-Page-Content
